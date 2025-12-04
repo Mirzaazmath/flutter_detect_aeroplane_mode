6b1de4c0eebd8e478fc51daba2c7764cbad0c345
Mirzaazmath <azmath9603399270@gmail.com>
2025-12-04 18:40:52 +0530

    added native code for android

--- android/app/src/main/kotlin/com/example/flutter_detect_aeroplane_mode/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_detect_aeroplane_mode/MainActivity.kt
@@ -1,5 +1,49 @@
 package com.example.flutter_detect_aeroplane_mode
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.provider.Settings
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.EventChannel
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterActivity() {
+    private val CHANNEL = "airplane_mode_events"
+    private var eventSink: EventChannel.EventSink? = null
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        EventChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            CHANNEL
+        ).setStreamHandler(object : EventChannel.StreamHandler {
+            override fun onListen(arguments: Any?, events: EventChannel.EventSink?) {
+                eventSink = events
+                registerAirplaneReceiver()
+            }
+
+            override fun onCancel(arguments: Any?) {
+                eventSink = null
+            }
+        })
+    }
+
+    private fun registerAirplaneReceiver() {
+        val filter = IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        registerReceiver(object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val isOn = Settings.Global.getInt(
+                    context.contentResolver,
+                    Settings.Global.AIRPLANE_MODE_ON,
+                    0
+                ) == 1
+
+                eventSink?.success(isOn)
+            }
+        }, filter)
+    }
+}
+
